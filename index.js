770d1c1b23d4543adee36bc8519b3b6b441014d1
micheaol <micheaol80@gmail.com>
2022-06-24 15:36:26 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors());
+
+
+const CONNECTION_URL = "mongodb+srv://codestation:codestation123456@cluster0.nmoqb1c.mongodb.net/?retryWrites=true&w=majority"
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, UseUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Successfully connected to DB, App running on PORT: ${PORT}`)))
+    .catch((error) => console.log(error.message) )
+

2004183b87e39a95b2bb8eab7318bfdd34ab3774
micheaol <micheaol80@gmail.com>
2022-06-27 10:32:26 +0100

    Add jwt to the project

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 import Users from "../models/user.js";
 import gravatar from "gravatar";
+import config from 'config';
+import jwt from 'jsonwebtoken';
 import bcrypt from "bcryptjs";
 
 
@@ -37,7 +39,23 @@ export const createUser =  async (req, res) => {
 
         await user.save()
         
-        res.send("User created succeful")
+       const payload = {
+           user: {
+               id: user.id
+           }
+       }
+
+       jwt.sign(
+           payload,
+           config.get('jwtSecretToken'),
+           {expiresIn: 3600000},
+           (err, token) => {
+            if(err) throw err;
+
+            res.json({ token})
+           }
+
+        );
 
     } catch (error) {
         console.log(error.message)
